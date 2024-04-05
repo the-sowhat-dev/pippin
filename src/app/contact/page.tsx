c70caa20d9f565549f383f860bd46c9004e95c6d
hbayoud <hugo.bayoud@gmail.com>
2024-04-05 14:11:32 +0200

    Rework on website metadata

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,45 +2,46 @@ import Link from 'next/link';
 import Image from 'next/image';
 import ContactProfile from '../../components/contact-profile';
 import Head from 'next/head';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Sowhat | Nous contacter',
+  description: 'Nous contacter par email ou directement sur Linkedin.',
+};
 
 export default function Page() {
   return (
-    <>
-      <Head>
-        <title>Sowhat : Nous contacter</title>
-      </Head>
-      <div className="min-h-screen bg-gradient-to-b from-yellow-400 to-yellow-300 flex flex-col">
-        <div className="w-full py-4 p-8">
-          <Link href="/">
-            <Image
-              width={32}
-              height={32}
-              style={{ objectFit: 'contain' }}
-              src="/icons/arrow-left.svg"
-              alt="Back arrow left icon"
-            />
-          </Link>
-        </div>
-        <main className="grid md:grid-cols-2 place-items-center flex-grow">
-          <ContactProfile
-            imagePath="/images/raph.png"
-            imageAlt="Raphael Metrop profile image"
-            title="Co-founder & CEO"
-            name="Raphaël METROP"
-            link="rapha%C3%ABl-metrop-05714323"
-            email="raphael.metrop@sowhat-app.com"
+    <div className="min-h-screen bg-gradient-to-b from-yellow-400 to-yellow-300 flex flex-col">
+      <div className="w-full py-4 p-8">
+        <Link href="/">
+          <Image
+            width={32}
+            height={32}
+            style={{ objectFit: 'contain' }}
+            src="/icons/arrow-left.svg"
+            alt="Back arrow left icon"
           />
-
-          <ContactProfile
-            imagePath="/images/hugo.png"
-            imageAlt="Hugo Bayoud profile image"
-            title="Co-founder & CTO"
-            name="Hugo BAYOUD"
-            link="hugo-bayoud-4aa927194"
-            email="hugo.bayoud@sowhat-app.com"
-          />
-        </main>
+        </Link>
       </div>
-    </>
+      <main className="grid md:grid-cols-2 place-items-center flex-grow">
+        <ContactProfile
+          imagePath="/images/raph.png"
+          imageAlt="Raphael Metrop profile image"
+          title="Co-founder & CEO"
+          name="Raphaël METROP"
+          link="rapha%C3%ABl-metrop-05714323"
+          email="raphael.metrop@sowhat-app.com"
+        />
+
+        <ContactProfile
+          imagePath="/images/hugo.png"
+          imageAlt="Hugo Bayoud profile image"
+          title="Co-founder & CTO"
+          name="Hugo BAYOUD"
+          link="hugo-bayoud-4aa927194"
+          email="hugo.bayoud@sowhat-app.com"
+        />
+      </main>
+    </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,22 @@
 import '../utils/globals.css';
 import { poppins } from '../utils/fonts';
 import type { Metadata } from 'next';
-import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: `Sowhat | Votre indépendance par l'Open Finance`,
   description:
     'Découvrez la solution innovante, basée sur l’Open Finance, qui révolutionne l’expérience de gestion et facilite la prise de décision pour vos finances personnelles.',
+  openGraph: {
+    title: 'Sowhat Application',
+    description: "Votre indépendance par l'Open Finance.",
+    images: { url: '../../public/images/logo-blanc.png' },
+    url: 'https://www.sowhat-app.com',
+  },
+  twitter: {
+    title: 'Sowhat Application',
+    description: "Votre indépendance par l'Open Finance.",
+    images: { url: '../../public/images/logo-blanc.png' },
+  },
 };
 
 export default function RootLayout({
@@ -15,24 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <html lang="fr">
-        <Head>
-          <title>Sowhat App - Votre indépendance par l&apos; Open Finance</title>
-          <meta
-            name="description"
-            content="Découvrez la solution innovante, basée sur l’Open Finance, qui révolutionne l’expérience de gestion et facilite la prise de décision pour vos finances personnelles."
-            key="desc"
-          />
-          <link rel="icon" href="./favicon.ico" />
-          <link rel="shortcut icon" href="./favicon.ico" />
-          <meta property="og:title" content="Sowhat Application" />
-          <meta property="og:url" content="https://www.sowhat-app.com" />
-          <meta property="og:description" content="Votre indépendance par l'Open Finance." />
-          <meta property="og:image" content="../../public/images/logo-blanc.png" />
-        </Head>
-        <body className={`${poppins.className}`}>{children}</body>
-      </html>
-    </>
+    <html lang="fr">
+      <body className={`${poppins.className}`}>{children}</body>
+    </html>
   );
 }
